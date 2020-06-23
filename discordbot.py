1824fbc866a25c981c7ba0d93fbb822c1795c528
shion8 <67339255+shion8@users.noreply.github.com>
2020-06-24 07:02:06 +0900

    implement neko

--- discordbot.py
+++ discordbot.py
@@ -15,11 +15,11 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('peko')
+    await ctx.send('pong')
     
 @bot.command()
-async def usada(ctx):
-    await ctx.send('ぺこ')
+async def neko(ctx):
+    await ctx.send('にゃーん')
 
 
 
